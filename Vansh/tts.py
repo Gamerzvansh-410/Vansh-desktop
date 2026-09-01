8b45e02eabcf5966807b135579414a1ede449fcf
Gamerzvansh-410 <vanshpatil76@gmail.com>
2026-09-01 20:14:35 +0000

    Changes to be committed: 	modified:   Vansh/tts.py  Changes not staged for commit: 	modified:   README.md 	deleted:    Vansh/__inti__.py 	modified:   Vansh/__pycache__/config.cpython-312.pyc 	modified:   Vansh/config.py 	modified:   Vansh/stt.py 	modified:   Vansh/tts.py 	modified:   main.py 	modified:   server.py  Untracked files: 	Vansh/__init__.py 	Vansh/__pycache__/__init__.cpython-312.pyc

--- Vansh/tts.py
+++ Vansh/tts.py
@@ -1,21 +1,38 @@
 import asyncio
 import os
 import tempfile
+
 import edge_tts
 from playsound import playsound
+
 from Vansh.config import EDGE_VOICE, EDGE_RATE
- 
-_AUDIO_FILE = os.path.join(tempfile.gettempdir(),"Vansh_voice.mp3")
+
+
+_AUDIO_FILE = os.path.join(
+    tempfile.gettempdir(),
+    "Vansh_voice.mp3"
+)
+
 
 async def _generate_audio(text: str):
-    communicate = edge_tts.Communicate(text, voice=EDGE_VOICE, rate=EDGE_RATE)
+    communicate = edge_tts.Communicate(
+        text,
+        voice=EDGE_VOICE,
+        rate=EDGE_RATE
+    )
+
     await communicate.save(_AUDIO_FILE)
 
 
-async def speak(text: str):
+def speak(text: str):
     print(f"Vansh: {text}")
+
     try:
         asyncio.run(_generate_audio(text))
         playsound(_AUDIO_FILE)
-    except Exception as e: 
-        print(f"Error generating or playing audio please check your internet connection and the edge_tts library. Error: {e}")
+
+    except Exception as e:
+        print(
+            "Error generating or playing audio. "
+            f"Error: {e}"
+        )
